cb1d7eb72ef0af4ba0801496dcc34d1e6db6d01c
Renaldyyy <renaldyb71@mail.ugm.ac.id>
2022-09-15 01:39:24 +0700

    edit if statement

--- js/script.js
+++ js/script.js
@@ -12,38 +12,37 @@ function masuk() {
         return false;
     } 
 
-
-    if (semester >= 1 && semester <= 2) {
+    else if (semester >= 1 && semester <= 2) {
         pesan = 'Anda mahasiswa tahun pertama, Mahasiswa Baru!';
         document.getElementById('pesann').innerHTML=pesan;
         return false
     }
 
-    if (semester >= 3 && semester <= 4) {
+    else if (semester >= 3 && semester <= 4) {
         pesan = 'Anda mahasiswa tahun kedua, Semangat!';
         (document.getElementById('pesann').innerHTML=pesan);
         return false
     }
 
-    if (semester >= 5 && semester <= 6) {
+    else if (semester >= 5 && semester <= 6) {
         pesan = 'Anda mahasiswa tahun ketiga, Tetap Semangat!';
         document.getElementById('pesann').innerHTML=pesan;
         return false
     }
 
-    if (semester >= 7 && semester <= 8) {
+    else if (semester >= 7 && semester <= 8) {
         pesan = 'Anda mahasiswa tahun keempat, Lulus Tahun Ini!';
         document.getElementById('pesann').innerHTML=pesan;
         return false
     }
 
-    if (semester <= 0) {
+    else if (semester <= 0) {
         pesan = 'Anda Bukan Mahasiswa di Universitas ini!';
         document.getElementById('pesann').innerHTML=pesan;
         return false
     }
 
-    if (semester >= 9) {
+    else if (semester >= 9) {
         pesan = 'Anda Bukan Mahasiswa di Universitas ini!';
         document.getElementById('pesann').innerHTML=pesan;
         return false
