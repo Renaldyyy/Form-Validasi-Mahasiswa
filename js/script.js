34b861b9350be5a54237e055e90e9b29a9a349c1
Renaldyyy <renaldyb71@mail.ugm.ac.id>
2022-09-15 01:24:34 +0700

    edit conditional statement

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 function masuk() {
     var nama = document.getElementById("nama").value;
     var semester = document.getElementById("smt").value;
+    var pesan = ''
     swal.setActionValue(semester);
     if (nama == '') {
         swal("Maaf", "Tolong isi Nama Anda", "warning");
@@ -9,24 +10,43 @@ function masuk() {
     } else if (semester== '') {
         swal("Maaf", "“isikan semester!!", "warning");
         return false;
-    } else if (semester >= 1 && semester <= 2) {
-        swal("Anda mahasiswa tahun pertama", "Mahasiswa Baru!", "success");
-        return false;
-    } else if (semester >= 3 && semester <= 4) {
-        swal("Anda mahasiswa tahun kedua", "Semangat!", "success");
-        return false;
-    } else if (semester >= 5 && semester <= 6) {
-        swal("Anda mahasiswa tahun ketiga", "Tetap Semangat!", "success");
-        return false;
-    } else if (semester >= 7 && semester <= 8) {
-        swal("Anda mahasiswa tahun keempat", "Lulus Tahun Ini!", "success");
-        return false;
-    } else if (semester <=0 ) {
-        swal("Maaf!", "Anda Bukan Mahasiswa di Universitas ini!", "warning");
-        return false;
-    } else if (semester >=9 ) {
-        swal("Maaf!", "Anda Bukan Mahasiswa di Universitas ini!", "warning");
-        return false;
+    } 
+
+
+    if (semester >= 1 && semester <= 2) {
+        pesan = 'Anda mahasiswa tahun pertama, Mahasiswa Baru!';
+        document.getElementById('pesann').innerHTML=pesan;
+        return false
+    }
+
+    if (semester >= 3 && semester <= 4) {
+        pesan = 'Anda mahasiswa tahun kedua, Semangat!';
+        (document.getElementById('pesann').innerHTML=pesan);
+        return false
+    }
+
+    if (semester >= 5 && semester <= 6) {
+        pesan = 'Anda mahasiswa tahun ketiga, Tetap Semangat!';
+        document.getElementById('pesann').innerHTML=pesan;
+        return false
+    }
+
+    if (semester >= 7 && semester <= 8) {
+        pesan = 'Anda mahasiswa tahun keempat, Lulus Tahun Ini!';
+        document.getElementById('pesann').innerHTML=pesan;
+        return false
+    }
+
+    if (semester <= 0) {
+        pesan = 'Anda Bukan Mahasiswa di Universitas ini!';
+        document.getElementById('pesann').innerHTML=pesan;
+        return false
+    }
+
+    if (semester >= 9) {
+        pesan = 'Anda Bukan Mahasiswa di Universitas ini!';
+        document.getElementById('pesann').innerHTML=pesan;
+        return false
     }
 
     document.getElementById("submit").addEventListener("click", function (event) {
